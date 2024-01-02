1309d699107509ef8f0a454badde48ba3eda1430
boumedjirek <haroun902009@hotmail.fr>
2024-01-02 23:39:19 +0100

    input training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
- import { AppComponent } from './app.component';  // Assurez-vous que le chemin d'accès au composant est correct
+ import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';  // Assurez-vous que le chemin d'accès au composant est correct
  
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChildComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
